a90a6bf29493d0e0426f4af08cd51acec2f44a7c
Fernando Álisson <fernando.alisson@ufpe.br>
2025-08-02 11:26:00 -0300

    Init: Start the project with docker, database and conetion with mysql

---
+++ src/server.js
@@ -0,0 +1,16 @@
+const app = require('./app');
+const connection = require('./db/connection');
+
+const PORT = 3001;
+
+app.listen(PORT, async () => {
+  console.log(`The app is running on port ${PORT}`)
+
+  // Esse trecho de código ficará responsável para saber se aplicação foi conectada 
+  // ao banco de dados MySQL, Uma vez testado não precisa ser executado mais. 
+   
+  // const [result] = await connection.execute('SELECT 1')
+  
+  // if (result) console.log('MySQL connection OK')
+});
+
